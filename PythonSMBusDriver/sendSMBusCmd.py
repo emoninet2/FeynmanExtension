01901afc3210a94cdf12ca049728c62d3a2fc6e9
Emon <emon.1992@live.com>
2017-08-29 00:00:38 +0000

    changed sendSMBusCmd.py. Now takes 2 arguments where -s is to send command and -r is planned to reset the extended module

--- PythonSMBusDriver/sendSMBusCmd.py
+++ PythonSMBusDriver/sendSMBusCmd.py
@@ -10,18 +10,20 @@ def main():
 	bus = smbus.SMBus(1)
 	FyenmanExtDevAddr = 0x08
 
-
-	data = []
-	for d in sys.argv[1]:
-		data.append(ord(d))
-		#print hex(ord(d))
-	data.append(10)
-	#print(data)
-
-	for i in data:
-		bus.write_byte(FyenmanExtDevAddr, i)
-
-
+	if(sys.argv[1] == "-s"):
+		data = []
+		for d in sys.argv[2]:
+			data.append(ord(d))
+			#print hex(ord(d))
+		data.append(10)
+		#print(data)
+	
+		for i in data:
+			bus.write_byte(FyenmanExtDevAddr, i)
+	elif(sys.argv[1] == "-r");
+		print("reset");
+	else:
+		print("unknown command")
 
 if __name__ == "__main__":
     main()
